802bbd8bab14823a24e341b4fa4bafa2751aec49
venkateshkattaboina23-blip <venkateshkattaboina23@gmail.com>
2026-07-19 15:12:07 +0530

    Create 1295.FIND NUMBERS WITH EVEN NUMBER OF DIGITS.JAVA

---
+++ 1295.FIND NUMBERS WITH EVEN NUMBER OF DIGITS.JAVA
@@ -0,0 +1,16 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+        int ans=0;
+        
+        for(int num:nums){
+            int digit=0;
+            while(num!=0){
+                digit++;
+                num=num/10;
+            }
+            if(digit%2==0)
+            ans++;
+        }
+        return ans;
+    }
+}
